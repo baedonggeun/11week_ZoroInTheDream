bc12e4043696d081924490bdc545837ebea3e9c0
2hys <tenjin62@gmail.com>
2024-02-14 01:19:24 +0900

    [Fix]_240214 스크립트 등 복구

--- Assets/MAP/Scripts/MapLogic.cs
+++ Assets/MAP/Scripts/MapLogic.cs
@@ -1,38 +1,36 @@
 using System.Collections;
 using System.Collections.Generic;
-using UnityEditor.Overlays;
 using UnityEngine;
 using UnityEngine.Tilemaps;
 
 public class MapLogic : MonoBehaviour
 {
     public Tilemap[] tilemaps;
-    public GameObject[] stage;
-    public GameObject NextStage; // 다음 스테이지를 가리키는 게임 오브젝트
-    public bool doorSpawned = false; // 문이 생성되었는지 여부를 나타냄
-    public Vector3 doorPosition;
-    private int monsterKillCount = 0; // 처치한 몬스터의 수를 저장
+    public GameObject NextStagedoor;
+    public bool doorSpawned = false;
+    private int monsterKillCount = 0;
 
     private void Update()
     {
-        if (Input.GetKeyDown(KeyCode.W)) //  플레이어가 'W' 키를 눌렀고 이미 문이 생성되었으며 몬스터를 충분히 처치했다면 다음 스테이지로 이동
+        if (Input.GetKeyDown(KeyCode.UpArrow))
         {
             if (doorSpawned && monsterKillCount >= 20)
             {
-                int randomStageIndex = Random.Range(0, stage.Length);
-                GameObject nextStage = stage[randomStageIndex];
-
-                Instantiate(nextStage, doorPosition, Quaternion.identity);
+                int randomIndex = Random.Range(0, tilemaps.Length);
+                Transform playerTransform = GameObject.FindGameObjectWithTag("Player").transform;
+                playerTransform.position = tilemaps[randomIndex].WorldToCell(Vector3.zero);
             }
         }
     }
 
-    public void MonsterKilled() // 몬스터를 처치한 수를 증가
+    public void MonsterKilled()
     {
         monsterKillCount++;
         if (monsterKillCount >= 20 && !doorSpawned)
         {
-            Instantiate(NextStage, doorPosition, Quaternion.identity);
+            Vector3 randomDoorPosition = new Vector3(Random.Range(-10f, 10f), Random.Range(-10f, 10f), 0f);
+
+            Instantiate(NextStagedoor, randomDoorPosition, Quaternion.identity);
             doorSpawned = true;
         }
     }
