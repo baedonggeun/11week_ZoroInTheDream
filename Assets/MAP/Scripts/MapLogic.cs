c5d8eab1bc5412c2fe2e8863fd5e1dfd3429bd3c
dch1114 <dch1114@naver.com>
2024-02-13 20:47:22 +0900

    [Add]240213_주석 처리

--- Assets/MAP/Scripts/MapLogic.cs
+++ Assets/MAP/Scripts/MapLogic.cs
@@ -8,14 +8,14 @@ public class MapLogic : MonoBehaviour
 {
     public Tilemap[] tilemaps;
     public GameObject[] stage;
-    public GameObject NextStage;
-    public bool doorSpawned = false;
+    public GameObject NextStage; // 스테이지를 가리키는 게임 오브젝트
+    public bool doorSpawned = false; // 문이 생성되었는지 여부를 나타냄
     public Vector3 doorPosition;
-    private int monsterKillCount = 0;
+    private int monsterKillCount = 0; // 처치한 몬스터의 수를 저장
 
     private void Update()
     {
-        if (Input.GetKeyDown(KeyCode.W))
+        if (Input.GetKeyDown(KeyCode.W)) // 플레이어가 'W' 키를 눌렀고 이미 문이 생성되었으며 몬스터를 충분히 처치했다면 다음 스테이지로 이동
         {
             if (doorSpawned && monsterKillCount >= 20)
             {
@@ -27,7 +27,7 @@ public class MapLogic : MonoBehaviour
         }
     }
 
-    public void MonsterKilled()
+    public void MonsterKilled() // 몬스터를 처치한 수를 증가
     {
         monsterKillCount++;
         if (monsterKillCount >= 20 && !doorSpawned)
--- Assets/MAP/Scripts/RandomMap.cs
+++ Assets/MAP/Scripts/RandomMap.cs
@@ -6,7 +6,7 @@ public class RandomMap : MonoBehaviour
 {
     private void OnTriggerEnter2D(Collider2D other)
     {
-        if (other.CompareTag("Player"))
+        if (other.CompareTag("Player")) // 충돌한 객체가 "Player" 태그를 가지고 있는지 확인 만약 플레이어와 충돌했다면 다음을 실행
         {
             MapLogic gameManager = FindObjectOfType<MapLogic>();
             if (gameManager != null && gameManager.GetComponent<MapLogic>().doorSpawned)
