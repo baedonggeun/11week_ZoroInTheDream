19cc8cc345ecdd99715bf646a51811dacd619ff6
2hys <tenjin62@gmail.com>
2024-02-14 19:22:16 +0900

    [Add]_240214 Staff 장착

--- Assets/Item/Scripts/bullet.cs
+++ Assets/Item/Scripts/bullet.cs
@@ -27,9 +27,9 @@ public class bullet : MonoBehaviour
     {
         if (collision.transform.CompareTag("Monster") || collision.transform.CompareTag("Wall"))
         {
-            animator.SetTrigger("isHit");
+            //animator.SetTrigger("isHit");
             Debug.Log("ИэСп!");
-            DestoryBullet();
+            Destroy(gameObject);
         }
     }
 
