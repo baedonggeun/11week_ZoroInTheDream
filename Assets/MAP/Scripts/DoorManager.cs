3a867bac9ad93dfa8e88d32396afb7b94012b055
dch1114 <dch1114@naver.com>
2024-02-15 17:47:42 +0900

    [fix]240215_수정

--- Assets/MAP/Scripts/DoorManager.cs
+++ Assets/MAP/Scripts/DoorManager.cs
@@ -3,7 +3,8 @@ using UnityEngine;
 public class DoorManager : MonoBehaviour
 {
     public GameObject doorPrefab;
-    public GameObject[] maps;
+    public GameObject[] regularRooms; // 보스 방을 제외한 일반 방
+    public GameObject bossRoom; // 보스 방
 
     private bool allMonstersDefeated = false;
 
@@ -31,38 +32,52 @@ public class DoorManager : MonoBehaviour
     // 문을 활성화하는 함수
     private void ActivateDoor()
     {
-        foreach (GameObject map in maps)
-        {
-            if (map.activeSelf)
-            {
-                Vector3 doorPosition = CalculateDoorPosition(map);
-                GameObject newDoor = Instantiate(doorPrefab, doorPosition, Quaternion.identity, transform);
-                newDoor.SetActive(true);
-                return;
-            }
-        }
+        GameObject[] rooms = GetRoomsToChooseFrom();
+        int randomIndex = Random.Range(0, rooms.Length);
+        GameObject randomRoom = rooms[randomIndex];
+
+        Vector3 doorPosition = CalculateDoorPosition(randomRoom);
+        GameObject newDoor = Instantiate(doorPrefab, doorPosition, Quaternion.identity, transform);
+        newDoor.SetActive(true);
     }
 
     // 문의 위치를 계산하는 함수 (각 맵마다 다른 위치에 배치)
-    private Vector3 CalculateDoorPosition(GameObject map)
+    private Vector3 CalculateDoorPosition(GameObject room)
     {
-        return map.transform.position;
+        return room.transform.position;
+    }
+
+    // 랜덤하게 선택할 방들의 배열 반환
+    private GameObject[] GetRoomsToChooseFrom()
+    {
+        // 마지막 방이 보스 방이 되도록 설정
+        GameObject[] rooms = new GameObject[regularRooms.Length + 1];
+        for (int i = 0; i < regularRooms.Length; i++)
+        {
+            rooms[i] = regularRooms[i];
+        }
+        rooms[rooms.Length - 1] = bossRoom;
+
+        return rooms;
     }
 
     // 문을 통과할 때 호출되는 함수
     public void PassThroughDoor()
     {
-        // 랜덤하게 맵 선택
-        int randomIndex = Random.Range(0, maps.Length);
-        GameObject randomMap = maps[randomIndex];
-
-        // 선택된 맵을 활성화하고 나머지는 비활성화
-        foreach (GameObject map in maps)
-        {
-            map.SetActive(map == randomMap);
-        }
-
         // 모든 몬스터가 처지지 않은 상태로 초기화
         allMonstersDefeated = false;
+    }
+
+
+    private Vector3 GetRandomPositionInMap(GameObject map)
+    {
+        // 맵의 중앙으로부터 일정 범위 내의 랜덤한 위치를 반환
+        float rangeX = 5f; // X 축 범위
+        float rangeY = 10f; // Y 축 범위
+        Vector3 mapCenter = map.transform.position;
+        float randomX = Random.Range(mapCenter.x - rangeX, mapCenter.x + rangeX);
+        float randomY = Random.Range(mapCenter.y - rangeY, mapCenter.y + rangeY);
+        float z = 0f;
+        return new Vector3(randomX, randomY, z);
     }
 }
